b6b87e6340c717023be72921999c11d11d7d6f75
devdav-id <179027516+devdav-id@users.noreply.github.com>
2025-09-10 13:55:43 -0400

    creation of files

--- bootscore-child/functions.php
+++ bootscore-child/functions.php
@@ -18,15 +18,37 @@ require_once get_stylesheet_directory() . '/aw-theme/wordpress-update-notice.php
 add_action('wp_enqueue_scripts', 'bootscore_child_enqueue_styles');
 function bootscore_child_enqueue_styles() {
 
-  // Compiled main.css
-  $modified_bootscoreChildCss = date('YmdHi', filemtime(get_stylesheet_directory() . '/assets/css/main.css'));
-  wp_enqueue_style('main', get_stylesheet_directory_uri() . '/assets/css/main.css', array('parent-style'), $modified_bootscoreChildCss);
-
-  // style.css
+  // Parent theme style
   wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
   
-  // custom.js
-  // Get modification time. Enqueue file with modification date to prevent browser from loading cached scripts when file content changes. 
-  $modificated_CustomJS = date('YmdHi', filemtime(get_stylesheet_directory() . '/assets/js/custom.js'));
-  wp_enqueue_script('custom-js', get_stylesheet_directory_uri() . '/assets/js/custom.js', array('jquery'), $modificated_CustomJS, false, true);
+  // Check if compiled main.css exists
+  $main_css_path = get_stylesheet_directory() . '/assets/css/main.css';
+  if (file_exists($main_css_path)) {
+    // Compiled main.css (includes AW SCSS)
+    $modified_bootscoreChildCss = date('YmdHi', filemtime($main_css_path));
+    wp_enqueue_style('main', get_stylesheet_directory_uri() . '/assets/css/main.css', array('parent-style'), $modified_bootscoreChildCss);
+    $main_handle = 'main';
+  } else {
+    // Fallback to child theme style.css
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'), wp_get_theme()->get('Version'));
+    $main_handle = 'child-style';
+  }
+
+  // AW Custom CSS
+  if (file_exists(get_stylesheet_directory() . '/assets/aw-css/aw-custom.css')) {
+    $modified_awCustomCss = date('YmdHi', filemtime(get_stylesheet_directory() . '/assets/aw-css/aw-custom.css'));
+    wp_enqueue_style('aw-custom', get_stylesheet_directory_uri() . '/assets/aw-css/aw-custom.css', array($main_handle), $modified_awCustomCss);
+  }
+  
+  // Original custom.js
+  if (file_exists(get_stylesheet_directory() . '/assets/js/custom.js')) {
+    $modificated_CustomJS = date('YmdHi', filemtime(get_stylesheet_directory() . '/assets/js/custom.js'));
+    wp_enqueue_script('custom-js', get_stylesheet_directory_uri() . '/assets/js/custom.js', array('jquery'), $modificated_CustomJS, true);
+  }
+  
+  // AW Custom JavaScript
+  if (file_exists(get_stylesheet_directory() . '/assets/aw-js/aw-custom.js')) {
+    $modified_awCustomJs = date('YmdHi', filemtime(get_stylesheet_directory() . '/assets/aw-js/aw-custom.js'));
+    wp_enqueue_script('aw-custom-js', get_stylesheet_directory_uri() . '/assets/aw-js/aw-custom.js', array('jquery'), $modified_awCustomJs, true);
+  }
 }
