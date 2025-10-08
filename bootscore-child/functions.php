321bd5bdc3873d0919e1b5da4a5ab69ed6b4061f
devdav-id <179027516+devdav-id@users.noreply.github.com>
2025-10-08 16:03:39 -0400

    commented out sass, added bootscore visual actions, and added back img/logo for homepage defaults

--- bootscore-child/functions.php
+++ bootscore-child/functions.php
@@ -30,3 +30,50 @@ function bootscore_child_enqueue_styles() {
   $modificated_CustomJS = date('YmdHi', filemtime(get_stylesheet_directory() . '/assets/js/custom.js'));
   wp_enqueue_script('custom-js', get_stylesheet_directory_uri() . '/assets/js/custom.js', array('jquery'), $modificated_CustomJS, false, true);
 }
+
+/**
+ * Generate hooks for all Bootscore actions
+ */
+$bootscore_hooks = [
+  'bootscore_before_masthead',
+  'bootscore_after_masthead_open',
+  'bootscore_before_navbar_brand',
+  'bootscore_after_navbar_brand',
+  'bootscore_after_nav_toggler',
+  'bootscore_before_masthead_close',
+  'bootscore_after_masthead',
+  'bootscore_after_primary_open',
+  'bootscore_before_title',
+  'bootscore_after_title',
+  'bootscore_before_loop',
+  'bootscore_before_loop_item',
+  'bootscore_before_loop_title',
+  'bootscore_after_loop_title',
+  'bootscore_loop_item_after_card_body',
+  'bootscore_after_loop_item',
+  'bootscore_after_loop',
+  'bootscore_after_featured_image',
+  'bootscore_before_entry_footer',
+  'bootscore_before_sidebar_widgets',
+  'bootscore_after_sidebar_widgets',
+  'bootscore_before_my_offcanvas_account',
+  'bootscore_before_footer',
+  'bootscore_footer_columns_before_container',
+  'bootscore_footer_columns_after_container_open',
+  'bootscore_footer_columns_before_container_close',
+  'bootscore_footer_columns_after_container',
+  'bootscore_footer_info_after_container_open',
+  'bootscore_before_mini_cart_footer',
+];
+
+// Add hooks dynamically
+foreach ($bootscore_hooks as $hook) {
+  add_action($hook, function ($location = null) use ($hook) {
+    $output = '<div class="alert alert-warning"><code>' . esc_html($hook) . '</code>';
+    if ($location) {
+      $output .= ' (Location: <strong>' . esc_html($location) . '</strong>)';
+    }
+    $output .= '</div>';
+    echo $output;
+  });
+}
